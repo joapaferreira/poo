3001ed9c42bc57926d929c0f659cd4bc2557fce6
José Aparecido <jose.aparecido@sisteminas.com.br>
2020-09-24 11:57:36 -0300

    2º commit

---
+++ Poo/src/poo/Poo.java
@@ -0,0 +1,10 @@
+package poo;
+
+import javax.swing.JOptionPane;
+
+public class Poo {
+	public static void main (String[] args) {
+		JOptionPane.showMessageDialog(null, "Bem Vindo ao Java");
+	}
+
+}
